f7d3ab660110a1d1bd231b94bb6789262c184a2b
Orange1110 <s1103014@gm.pu.edu.tw>
2022-12-17 20:45:21 +0800

    version 0

--- index.py
+++ index.py
@@ -36,7 +36,7 @@ def item():
 
         if result == "":
             result = "是怎樣?皮啊?給我重輸"
-        return  result + render_template("item.html")
+        return  result + render_template("Question.html")
     else:
         return render_template("item.html")
 
